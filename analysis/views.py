ead657e856a8b12ae21d6c0222f25db67a819787
li xiangning <lixiangning@lis-MacBook-Pro.local>
2015-08-28 16:42:41 +0800

    added  username to comments object

--- analysis/views.py
+++ analysis/views.py
@@ -763,6 +763,7 @@ def comments(request, task_id):
             }
         buf["Data"] = "".join(escape_map.get(thechar, thechar) for thechar in comment)
         buf["UserID"] = request.user.id
+        buf["UserName"] = request.user.name
         curcomments.insert(0, buf)
         results_db.analysis.update({"info.id": int(task_id)},{"$set":{"info.comments":curcomments}}, upsert=False, multi=True)
         encrpt_task_id = until.encrpt(task_id)
